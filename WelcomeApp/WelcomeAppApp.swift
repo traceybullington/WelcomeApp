4410b83b540d732cfaf99de234523b2ca4b7f3e4
Tracey Bullington <traceylbullington@gmail.com>
2023-06-06 21:51:34 -0500

    Initial Commit

---
+++ WelcomeApp/WelcomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeAppApp.swift
+//  WelcomeApp
+//
+//  Created by scholar on 6/6/23.
+//
+
+import SwiftUI
+
+@main
+struct WelcomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
