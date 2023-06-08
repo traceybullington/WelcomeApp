23fa1a94bf3192a369dd16e86c097bf5ef95fb2e
Tracey Bullington <traceylbullington@gmail.com>
2023-06-07 21:55:21 -0500

    Practice for the GitHub lesson

--- WelcomeApp/ContentView.swift
+++ WelcomeApp/ContentView.swift
@@ -7,14 +7,25 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var textTitle = "what is your name"
+   @State private var name = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 50) {
+            Text(textTitle)
+                .font(.title)
+            TextField("type name here", text: $name)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+            Button("submit name") {
+                textTitle = "welcome, \(name)!"
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
         }
+        .padding()
         .padding()
     }
 }
